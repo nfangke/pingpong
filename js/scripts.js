c44ef65098c1a8ca4c7882a48b7170b2e91313bc
Nancy Fang <nfangke@gmail.com>
2016-04-01 14:10:08 -0700

    Display a mixed list of numbers and strings based on the user's input. Allow the user to clear results and submit again.

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+// BUSINESS LOGIC
+var userInput;
+var userInputArray = [];
+var replaceValArray = [];
+
+// determine if input is valid
+var ifNumber = function(userInput) {
+  if (userInput === NaN || userInput < 0 || userInput % 1 !==0) {
+  return false;
+  } else { return true;
+    }
+}
+
+// Creates a new array from 1 to n
+var countInput = function(userInput) {
+
+  for (i = 1; i < userInput + 1; i++) {
+    userInputArray.push(i);
+  }
+
+  // Replaces array items into the list
+  userInputArray.forEach(function(list) {
+    if (list % 15 === 0) {
+      $("ul").append("<li>" + "Ping Pong" + "</li>");
+    } else if (list % 5 === 0) {
+      $("ul").append("<li>" + "Pong" + "</li>");
+    } else if (list % 3 === 0) {
+      $("ul").append("<li>" + "Ping" + "</li>");
+    } else {
+      $("ul").append("<li>" + list + "</li>");
+    }
+  });
+
+};
+
+
+// UI Logic
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    $("#resetForm").show();
+    $("#submit").hide();
+
+    userInput = parseInt($("#userInput").val());
+
+    var resultIfNumber = ifNumber(userInput);
+
+    if (!resultIfNumber) {
+      alert("Please enter a whole number greater than zero");
+    } else {
+
+      countInput(userInput);
+      $("ul").show(this);
+    }
+
+
+  });
+
+  $("#resetForm").submit(function() {
+    window.location.reload();
+
+  });
+
+});
